8c79e991bdaeddb1631edb4dc2891a61511d4473
tacobayle <nicolas.bayle@gmail.com>
2022-01-05 10:48:47 +0000

    20220105_ubuntu_dns

--- output.tf
+++ output.tf
@@ -1,5 +1,9 @@
-output "ubuntu_static_ips" {
+output "ubuntu_ip" {
   value = var.dhcp == false ? var.ubuntu_ip4_addresses.* : vsphere_virtual_machine.ubuntu.*.default_ip_address
+}
+
+output "dns_ip" {
+  value = var.dhcp == false ? split("/", var.ubuntu_ip4_addresses[-1])[0] : vsphere_virtual_machine.dns.*.default_ip_address
 }
 
 output "ubuntu_username" {
@@ -7,6 +11,14 @@ output "ubuntu_username" {
 }
 
 output "ubuntu_password" {
+  value = var.ubuntu_password == null ? random_string.ubuntu_password.result : var.ubuntu_password
+}
+
+output "dns_username" {
+  value = var.ubuntu.username
+}
+
+output "dns_password" {
   value = var.ubuntu_password == null ? random_string.ubuntu_password.result : var.ubuntu_password
 }
 
