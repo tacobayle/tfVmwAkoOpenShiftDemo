24b6151e42b24b790d681cf5ff37da818fbc23f3
tacobayle <nicolas.bayle@gmail.com>
2022-01-05 10:12:41 +0000

    20220105_ubuntu

---
+++ output.tf
@@ -0,0 +1,15 @@
+output "ubuntu_static_ips" {
+  value = var.dhcp == false ? var.ubuntu_ip4_addresses.* : vsphere_virtual_machine.ubuntu.*.default_ip_address
+}
+
+output "ubuntu_username" {
+  value = var.ubuntu.username
+}
+
+output "ubuntu_password" {
+  value = var.ubuntu_password == null ? random_string.ubuntu_password.result : var.ubuntu_password
+}
+
+output "ssh_private_key_path" {
+  value = "~/.ssh/${var.ssh_key.private_key_name}.pem"
+}
