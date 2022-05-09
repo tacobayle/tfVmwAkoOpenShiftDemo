c61e8ed31f45ccb9c77c5ed07cbce5395518380e
tacobayle <nicolas.bayle@gmail.com>
2022-05-09 17:52:45 +0000

    20220905_avi

---
+++ avi_ansible.tf
@@ -0,0 +1,53 @@
+
+data "template_file" "avi_yaml_values" {
+  template = file("templates/avi_yaml_values.yml.template")
+  vars = {
+    controller_ip = vsphere_virtual_machine.controller_dhcp[0].default_ip_address
+    avi_password = var.avi_password
+    avi_old_password = var.avi_old_password
+    avi_version = var.avi_version
+    avi_username = var.avi_username
+    vsphere_username = var.vsphere_username
+    vsphere_password = var.vsphere_password
+    vsphere_server = var.vsphere_server
+    domains = jsonencode(var.avi.config.vcenter.domains)
+    dc = var.vcenter_dc
+    dchp_enabled = jsonencode(var.avi.config.vcenter.cloud.dhcp_enabled)
+    network_management = jsonencode(var.avi.config.vcenter.cloud.network_management)
+    network_vip = jsonencode(var.avi.config.vcenter.cloud.network_vip)
+    network_backend = jsonencode(var.avi.config.vcenter.cloud.network_backend)
+    service_engine_groups = jsonencode(var.avi.config.vcenter.cloud.service_engine_groups)
+    pools = jsonencode(var.avi.config.vcenter.pools)
+    pool_groups = jsonencode(var.avi.config.vcenter.pool_groups)
+    virtual_services = jsonencode(var.avi.config.vcenter.virtual_services)
+  }
+}
+
+resource "null_resource" "ansible" {
+  depends_on = [
+    vsphere_virtual_machine.ubuntu,
+    vsphere_virtual_machine.controller_dhcp]
+
+  connection {
+    host = vsphere_virtual_machine.ubuntu.default_ip_address
+    type = "ssh"
+    agent = false
+    user = "ubuntu"
+    private_key = tls_private_key.ssh.private_key_pem
+  }
+
+  provisioner "file" {
+    content = data.template_file.avi_yaml_values.rendered
+    destination = "avi_yaml_values.yml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p ansible",
+      "cd ~/ansible ; git clone ${var.ansible.aviConfigureUrl} --branch ${var.ansible.aviConfigureTag}; cd ${split("/", var.ansible.aviConfigureUrl)[4]}",
+      "ansible-playbook vcenter.yml --extra-vars @../avi_yaml_values.yml"
+    ]
+  }
+
+
+}
--- variables.tf
+++ variables.tf
@@ -10,6 +10,14 @@ variable "openshift_pull_secret" {}
 
 variable "avi_controller_url" {}
 
+variable "avi" {}
+variable "avi_password" {}
+variable "avi_old_password" {}
+variable "avi_username" {
+  default = "admin"
+}
+
+
 variable "vsphere_server" {
   default = "wdc-06-vc12.oc.vmware.com"
 }
@@ -62,6 +70,10 @@ variable "openshift_cluster_name" {
   default = "cluster1"
 }
 
+variable "avi_version" {
+  default = "21.1.4"
+}
+
 //variable "openshift_ubuntu_ip" {
 //  default = "100.64.129.210"
 //}
@@ -88,6 +100,17 @@ variable "dhcp" {
   default = true
 }
 
+variable "ansible" {
+  type = map
+  default = {
+    version = "2.10.7"
+    aviPbAbsentUrl = "https://github.com/tacobayle/ansibleAviClear"
+    aviPbAbsentTag = "v1.03"
+    aviConfigureTag = "v1.39"
+    aviConfigureUrl = "https://github.com/tacobayle/ansibleAviConfig"
+  }
+}
+
 variable "content_library" {
   default = {
     basename = "content_library_tf_"
@@ -101,7 +124,6 @@ variable "ubuntu" {
     basename = "ubuntu-tf-"
     username = "ubuntu"
     cpu = 8
-    if_name = "ens192"
     memory = 8192
     disk = 12
     wait_for_guest_net_routable = "false"
@@ -114,7 +136,6 @@ variable "dns" {
     basename = "dns-tf-"
     username = "ubuntu"
     cpu = 4
-    if_name = "ens192"
     memory = 4096
     disk = 12
     wait_for_guest_net_routable = "false"
@@ -132,5 +153,7 @@ variable "controller" {
     memory = 24768
     disk = 128
     wait_for_guest_net_timeout = 4
+    dns =  ["10.23.108.1", "10.23.108.2"]
+    ntp = ["95.81.173.155", "188.165.236.162"]
   }
 }
