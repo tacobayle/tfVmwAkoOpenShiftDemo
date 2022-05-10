66c9c1b50af3273d835ec3244ca5348ff1af12df
tacobayle <nicolas.bayle@gmail.com>
2022-05-10 15:09:07 +0000

    20220510_avi

--- variables.tf
+++ variables.tf
@@ -106,7 +106,7 @@ variable "ansible" {
     version = "2.10.7"
     aviPbAbsentUrl = "https://github.com/tacobayle/ansibleAviClear"
     aviPbAbsentTag = "v1.03"
-    aviConfigureTag = "v1.42"
+    aviConfigureTag = "v1.43"
     aviConfigureUrl = "https://github.com/tacobayle/ansibleAviConfig"
   }
 }
